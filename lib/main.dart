3d99c541d0b7eab90cf778f20bf36ca203382f69
Mahmoud Hafez Eltarqi <98850036+x-brymo@users.noreply.github.com>
2024-01-05 14:14:35 +0200

    updata

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:play_audio_widget_of_week/firstWeek/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+ 
   runApp( const MyAppFirstWeek());
 }
