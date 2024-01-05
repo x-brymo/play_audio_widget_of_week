05470e84e972e0db510059fefacb168008ad3736
Mahmoud Hafez Eltarqi <98850036+x-brymo@users.noreply.github.com>
2024-01-05 17:15:40 +0200

    pushed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
-import 'package:flutter/cupertino.dart';
-import 'package:play_audio_widget_of_week/firstWeek/app.dart';
+//import 'package:flutter/cupertino.dart';
+import 'package:alarm/alarm.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+import '2thweek/widget_2th_week.dart';
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
  
-  runApp( const MyAppFirstWeek());
+//   runApp( const MyAppFirstWeek());
+// }
+////[]
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  await Alarm.init(showDebugLogs: true);
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: false),
+      home: const ExampleAlarmHomeScreen(),
+    ),
+  );
 }
