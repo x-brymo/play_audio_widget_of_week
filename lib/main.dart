9f7937f61ee610b107d687a4b48352a047ebf9b3
Mahmoud Hafez Eltarqi <98850036+x-brymo@users.noreply.github.com>
2024-01-06 11:00:10 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ import 'package:easy_audio_player/helpers/init_just_audio_background.dart';
 import 'package:easy_audio_player/models/notification_configuration.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:play/play.dart';
 import 'package:play_audio_widget_of_week/3thweek/widget_3th_of_week.dart';
 import 'package:play_audio_widget_of_week/firstWeek/app.dart';
 import '2thweek/widget_2th_week.dart';
+import '3thweek/widget/first_test.dart';
+import '3thweek/widget/scand_test.dart';
 ////[MyAppFirstWeek]
 // void main() {
 //   WidgetsFlutterBinding.ensureInitialized();
@@ -29,10 +32,19 @@ import '2thweek/widget_2th_week.dart';
 // }
 
 ///[HomeScreen]
+// void main() async {
+//   // init the background service to display notifications while playing
+//   await initJustAudioBackground(NotificationSettings(androidNotificationChannelId: 'com.example.example'));
+//   runApp(MaterialApp(
+//     home: HomeScreen(),
+//   ));
+// }
+
+////[MainPage] 
 void main() async {
-  // init the background service to display notifications while playing
-  await initJustAudioBackground(NotificationSettings(androidNotificationChannelId: 'com.example.example'));
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  Play.ensureInitialized();
+  return runApp(
+    const MaterialApp(home: MainPage()),
+  );
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,20 @@
 #include "generated_plugin_registrant.h"
 
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
+#include <media_kit_libs_windows_video/media_kit_libs_windows_video_plugin_c_api.h>
+#include <media_kit_video/media_kit_video_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <screen_brightness_windows/screen_brightness_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
+  MediaKitLibsWindowsVideoPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitLibsWindowsVideoPluginCApi"));
+  MediaKitVideoPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  ScreenBrightnessWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenBrightnessWindowsPlugin"));
 }
