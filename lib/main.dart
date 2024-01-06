2aaaf64f762e7c91562f565c228dda52e563ba50
Mahmoud Hafez Eltarqi <98850036+x-brymo@users.noreply.github.com>
2024-01-06 09:57:03 +0200

    update-3th-week

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,38 @@
 //import 'package:flutter/cupertino.dart';
 import 'package:alarm/alarm.dart';
+import 'package:easy_audio_player/helpers/init_just_audio_background.dart';
+import 'package:easy_audio_player/models/notification_configuration.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:play_audio_widget_of_week/3thweek/widget_3th_of_week.dart';
 import 'package:play_audio_widget_of_week/firstWeek/app.dart';
 import '2thweek/widget_2th_week.dart';
-
+////[MyAppFirstWeek]
 // void main() {
 //   WidgetsFlutterBinding.ensureInitialized();
  
 //   runApp( const MyAppFirstWeek());
 // }
-////[]
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+////[ExampleAlarmHomeScreen]
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  await Alarm.init(showDebugLogs: true);
+//   await Alarm.init(showDebugLogs: true);
 
-  runApp(
-    MaterialApp(
-      theme: ThemeData(useMaterial3: false),
-      home: const ExampleAlarmHomeScreen(),
-    ),
-  );
+//   runApp(
+//     MaterialApp(
+//       theme: ThemeData(useMaterial3: false),
+//       home: const ExampleAlarmHomeScreen(),
+//     ),
+//   );
+// }
+
+///[HomeScreen]
+void main() async {
+  // init the background service to display notifications while playing
+  await initJustAudioBackground(NotificationSettings(androidNotificationChannelId: 'com.example.example'));
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
 }
